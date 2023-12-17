e64db7d50b3048adae587ece17e35b63a4742485
josepicoli <josepicoli42@gmail.com>
2023-12-17 17:33:21 -0300

    small refactoring in functions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 def text_format(en, pt):
+    en = en.strip()
+    pt = pt.strip()
     return f"{en}#{pt}"
 
 def add_bd(text):
-    for i in range(0,5):
-        with open("bd.txt", "a") as bd:
-            bd.write(f"{text}\n")
+    with open("bd.txt", "a") as bd:
+        bd.write(f"{text}\n")
 
 t = text_format("red", "vermelho")
 
