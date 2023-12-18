b9b2e78a4a176fbf8705b843a66534cc697bcff6
josepicoli <josepicoli42@gmail.com>
2023-12-18 19:58:22 -0300

    minimally functional reminder with command line

--- reminder.py
+++ reminder.py
@@ -1,21 +1,44 @@
 from tkinter import *
 from random import *
 
-def get_bd():
+def get_words():
     try:
         with open("bd.txt", "r") as bd:
-            return bd.readlines()
+            words = bd.readlines()
     except:
-        print("error reading the file")
-        return "404"
+        return print("error reading the file")
 
-def choose(words):
-    return choice(words)
+    words = choice(words)
+    words = words.split("=")
+    words[1] = words[1][:-1]
+    return words
 
 def interface():
+    black = "#000000"
+    white = "#ffffff"
+
     frame = Tk()
     frame.geometry("400x200")
+    frame.title("teste")
+    frame.config(bg= black)
+
+    lb_ask = Label(frame)
+    lb_ask.config(text= choose(get_bd()))
+    lb_ask.config(font= "Arial 18 bold")
+    lb_ask.config(bg= black)
+    lb_ask.config(fg= white)
+    lb_ask.place(x= 175, y= 20)
+
     frame.mainloop()
 
-print(choose(get_bd()))
-interface()
+def cli():
+    while True:
+        word = get_words()
+        print(f"question: {word[0]}")
+        response = input("response: ")
+        if response == word[1]:
+            print("T")
+        else:
+            print("F")
+
+cli()
