b55ba778cf7575bb95834800f299f4f22a11e6d6
josepicoli <josepicoli42@gmail.com>
2023-12-22 13:07:55 -0300

    removing graphical interfaces

--- register.py
+++ register.py
@@ -20,43 +20,6 @@ def number_words():
     except:
         return "I couldn't find your words"
 
-def interface():
-    black = "#000000"
-    white = "#ffffff"
-
-    frame = Tk()
-    frame.geometry("400x200")
-    frame.title("teste")
-    frame.config(bg= black)
-
-    lb_en = Label(frame)
-    lb_en.config(text= "English")
-    lb_en.config(bg= black)
-    lb_en.config(fg= white)
-    lb_en.place(x= 65, y= 50)
-
-    txt_en = Entry(frame)
-    txt_en.config(bd= 0)
-    txt_en.place(x= 150, y= 50)
-
-    lb_pt = Label(frame)
-    lb_pt.config(text= "português")
-    lb_pt.config(bg= black)
-    lb_pt.config(fg= white)
-    lb_pt.place(x= 65, y= 80)
-
-    txt_pt = Entry(frame)
-    txt_pt.config(bd= 0)
-    txt_pt.place(x= 150, y= 80)
-
-    add_button = Button(frame)
-    add_button.config(text= "ADD")
-    add_button.config(bd= 0)
-    add_button.config(command= lambda: add_bd(text_format(txt_en.get(), txt_pt.get())))
-    add_button.place(x= 190, y= 110)
-
-    frame.mainloop()
-
 def cli():
     while True:
         logo()
@@ -79,6 +42,5 @@ def cli():
         sleep(1)
         system("clear")
 
-#interface()
 system("clear")
 cli()
--- reminder.py
+++ reminder.py
@@ -17,24 +17,6 @@ def get_words():
     words[1] = words[1].replace("\n", "")
     return words
 
-def interface():
-    black = "#000000"
-    white = "#ffffff"
-
-    frame = Tk()
-    frame.geometry("400x200")
-    frame.title("teste")
-    frame.config(bg= black)
-
-    lb_ask = Label(frame)
-    lb_ask.config(text= "ask")
-    lb_ask.config(font= "Arial 18 bold")
-    lb_ask.config(bg= black)
-    lb_ask.config(fg= white)
-    lb_ask.place(x= 175, y= 20)
-
-    frame.mainloop()
-
 def cli(mode = 0):
     match mode:
         case 0:
